e0a22d82fc27a5174069c85cc8ae7632603dc314
Maxcs318 <maxkiller1112@gmail.com>
2019-08-13 17:23:35 +0700

    +article +course + book

--- client/src/components/Articles/articles.vue
+++ client/src/components/Articles/articles.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        article
+    <div class="container">
+        <h4 class="mt-3">บทความต่างๆ</h4> <br>
+        <div class="row">
+            <div class="col-lg-3 col-md-6" v-for="(article,index) in the_article.slice(0,4)" :key="index" >
+                <img :src="getImgUrlArticle(article.a_image)" width="100%">
+                <h5>{{article.a_title}}</h5>
+                <p style="text-align: right;">{{article.a_date}}</p>
+                <p style="text-indent: 2em;">{{article.a_detail.slice(0,60)}}</p>
+                <!-- <p style="text-align: right;">อ่านเพิ่มเติม</p> -->
+            </div>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    methods:{
+        getImgUrlArticle(pic) {
+            return require('../../assets/Article/'+pic)
+        }
+    },
+    computed:{
+        the_article(){
+            return this.$store.getters.getArticle
+        }
+    }
+}
+</script>
--- client/src/components/Training_Courses/training_courses.vue
+++ client/src/components/Training_Courses/training_courses.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        training course
+    <div class="container">
+        <h4 class="mt-3">หลักสูตร & อบรม</h4> <br>
+        <div class="row">
+            <div class="col-lg-3 col-xs-6" v-for="(product,index) in trainingC" :key="index">
+                <img :src="getImgUrl(product.p_image)" width="100%">
+                <h5>{{product.p_title}}</h5>
+                <p style="text-align: right;">{{product.p_date}}</p>
+                <p style="text-indent: 2em;">{{product.p_detail.slice(0,60)}}</p>
+                <!-- <p style="text-align: right;">อ่านเพิ่มเติม</p> -->
+            </div>
+        </div>
     </div>
 </template>
+<script>
+export default {
+    methods:{
+        getImgUrl(pic) {
+            return require('../../assets/Product/'+pic)
+        }
+    },
+    computed:{
+        trainingC(){
+            return this.$store.getters.getTrainingCourse
+        }
+    }
+}
+</script>
