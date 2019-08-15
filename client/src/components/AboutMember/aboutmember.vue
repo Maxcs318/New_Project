7829c3fa657ce49a5db180710d93fbd6951e8af0
Maxcs318 <maxkiller1112@gmail.com>
2019-08-15 17:10:14 +0700

    ++

--- client/src/components/AboutMember/aboutmember.vue
+++ client/src/components/AboutMember/aboutmember.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        about member
+    <div class="container mt-3">
+        <h4 class="mt-3">About Member</h4> <br>
+        <div class="row">
+            <div class="col-lg-12 col-xs-12">
+                about member
+            </div>
+        </div>
+        
     </div>
 </template>
--- client/src/components/Contact/contact.vue
+++ client/src/components/Contact/contact.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        contact
+    <div class="container mt-3">
+        <h4 class="mt-3">Contact US</h4> <br>
+        <div class="row">
+            <div class="col-lg-12 col-xs-12">
+                contact us
+            </div>
+        </div>
+        
     </div>
 </template>
