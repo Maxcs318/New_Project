36159adfd0592fc21dd936e732cffef1e6184e42
Maxcs318 <maxkiller1112@gmail.com>
2019-09-18 16:31:54 +0700

    ++

--- client/src/view/Shopping/order.vue
+++ client/src/view/Shopping/order.vue
@@ -6,9 +6,26 @@
             </div>
         </div>
         <div class="row" v-if="Order!=0">
-            {{ Order }}<br>
-            Status : {{ Order.o_status_id }}<br>
-            Order Create Date : {{ Order.o_create_date }}<br>
+            <div class="col-lg-12 col-xs-12">
+                <table style="width:100%; text-align: center;">
+                    <tr>
+                        <th> Product ID </th>
+                        <th> Price </th>
+                        <th> Quantity </th>
+                        <th> Total Price </th>
+                    </tr>
+                    <tr v-for="order_item in Order_Items">
+                        <td>{{order_item.oi_product_id}}</td>
+                        <td>{{order_item.oi_product_price}}</td>
+                        <td>{{order_item.oi_quantity}}</td>
+                        <td>{{order_item.oi_total_price}}</td>
+                    </tr>
+                </table>
+                <br>
+                Total Price : {{ Order.o_total_price }}<br>
+                Status : {{ Order.o_status_id }}<br>
+                Order Create Date : {{ Order.o_create_date }}<br>
+            </div>
         </div>
         <div class="row" v-else>
             <div class="col-lg-12 col-xs-12">
@@ -35,6 +52,17 @@ export default {
                 }
             }
             return 0 
+        },
+        Order_Items(){
+            var odi = this.$store.getters.getOrder_Item
+            var order_i=[]
+                for(var i=0;i<odi.length; i++){
+                    if(odi[i].oi_order_id == this.Order.o_id){
+                        order_i.push(odi[i])
+                    }
+                }
+            var product_all = this.$store.getters.getProduct_Set_Category
+            return order_i
         }
     },
     
